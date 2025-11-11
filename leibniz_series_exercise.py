8498b959c89c8f0cc2dc4bb8d4d9e2a5ed4a2565
yanko-coder <yannaykohn@gmail.com>
2025-11-11 22:41:26 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
   pi_est = 0
-  for i in range(0,n_terms+1):
+  for i in range(0,n_terms):
     pi_est += ((-1)**i)/(2*i+1)
   result = pi_est * 4
-  print(result)
+  return result
